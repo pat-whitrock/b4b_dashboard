eacb70bb3f2e4d6b36efc47fa99751bdb2d47ca2
Patrick Whitrock <patrick.whitrock@betterment.com>
2017-06-15 11:36:00 -0400

    Set up distribution dashboard card and dotenv

--- jobs/distribution_count.rb
+++ jobs/distribution_count.rb
@@ -1,3 +1,15 @@
+require 'net/http'
+require 'json'
+require 'dotenv/load'
+
+Dotenv.load
+
+DISTRIBUTION_COUNT_URI = URI(
+  "https://redash.betterment.com/api/queries/1734/results.json?api_key=#{ENV['REDASH_API_KEY']}"
+).freeze
+
 SCHEDULER.every '2s' do
-  send_event('distribution_count', { current: 100, previous: 100 })
+  distribution_count = JSON.parse(Net::HTTP.get(DISTRIBUTION_COUNT_URI))['query_result']['data']['rows'].first['count']
+
+  send_event('distribution_count', { current: distribution_count, previous: distribution_count })
 end
