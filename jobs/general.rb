c986c97c52735affe1e78b7f1b23e68a07cfc648
Patrick Whitrock <patrick.whitrock@betterment.com>
2017-06-15 12:20:38 -0400

    Top states by participant count

--- jobs/general.rb
+++ jobs/general.rb
@@ -7,9 +7,19 @@ Dotenv.load
 DISTRIBUTION_COUNT_URI = URI(
   "https://redash.betterment.com/api/queries/1734/results.json?api_key=#{ENV['REDASH_API_KEY']}"
 ).freeze
+PARTICIPANTS_BY_STATE_URI = URI(
+  "https://redash.betterment.com/api/queries/488/results.json?api_key=#{ENV['REDASH_API_KEY']}"
+).freeze
 
 SCHEDULER.every '2s' do
   distribution_count = JSON.parse(Net::HTTP.get(DISTRIBUTION_COUNT_URI))['query_result']['data']['rows'].first['count']
-
   send_event('distribution_count', { current: distribution_count, previous: distribution_count })
+
+  participants_by_state = JSON.parse(Net::HTTP.get(PARTICIPANTS_BY_STATE_URI))
+  top_states = participants_by_state['query_result']['data']['rows'].sort_by do |row|
+    -row['count']
+  end.take(15).map do |row|
+    { label: row['state'], value: row['count'] }
+  end
+  send_event('participants_by_state', { items: top_states })
 end
